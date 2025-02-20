93c280149a9cc4a567f447a545228e2b3b182412
fabriciobrito01 <fabriciodbe@gmail.com>
2025-02-20 17:01:59 -0300

    up ex03

--- Exercicios20-02-2024/src/Ex03.java
+++ Exercicios20-02-2024/src/Ex03.java
@@ -8,8 +8,7 @@ public class Ex03 {
         System.out.print("Digite o aumento: ");
         float aumentoPercentual = teclado.nextFloat();
 
-        float aumentoSalario = salario * aumentoPercentual;
-        float salarioFinal = salario + aumentoSalario;
-        System.out.printf("O novo salario eh %.2f", salarioFinal);
+        salario += salario * aumentoPercentual;
+        System.out.printf("O novo salario eh %.2f", salario);
     }
 }
